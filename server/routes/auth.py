6c572d28585e92f20b4bc7405ef7899ed7b256bb
kamusarihanshi <ahmedizaka9@gmail.com>
2024-07-28 14:41:08 +0300

    more resources

--- server/routes/auth.py
+++ server/routes/auth.py
@@ -3,16 +3,29 @@ from flask_restful import Api,Resource,reqparse
 from models import db,User
 from flask_cors import CORS
 from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
 
 
 auth_bp=Blueprint('user',__name__,url_prefix='/user')
 auth_api=Api(auth_bp)
 CORS(auth_bp)
 bcrypt=Bcrypt()
+jwt=JWTManager()
+
+Register_parser=reqparse.RequestParser()
+Register_parser.add_argument('username',type=str,required=True,help='Username is required')
+Register_parser.add_argument('email',type=str,required=True,help='Email is required')
+Register_parser.add_argument('password',type=str,required=True,help='Password is required')
 
 
-
-
+class Register(Resource):
+    def post(self):
+        data=Register_parser.parse_args()
+        hashed_password=bcrypt.generate_password_hash(data['password'])
+        new_user=User(username=data['username'],email=data['email'],password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return {'message':'User registered successfully'},201
 
 
 
--- server/routes/categories.py
+++ server/routes/categories.py
@@ -7,3 +7,32 @@ from flask_cors import CORS
 category_bp=Blueprint('category',__name__,url_prefix='/category')
 category_api=Api(category_bp)
 CORS(category_bp)
+
+category_parser=reqparse.RequestParser()
+category_parser.add_argument('name',type=str,required=True,help='Name is required')
+category_parser.add_argument('description',type=str,required=False,help='not required')
+
+class Category(Resource):
+    def get(self,id):
+        category=Categories.query.get(id)
+        return {'id':category.id, 'name':category.name, 'description':category.description}
+    def post(self):
+        data=category_parser.parse_args()
+        new_category=Categories(name=data['name'], description=data['description'])
+        db.session.add(new_category)
+        db.session.commit()
+        return {'message':'Category added successfully'},201
+    def put(self,id):
+        data=category_parser.parse_args()
+        category=Categories.query.get(id)
+        category.name=data['name']
+        category.description=data['description']
+        db.session.commit()
+        return {'message':'Category updated successfully'},200
+    def delete(self,id):
+        category=Categories.query.get(id)
+        db.session.delete(category)
+        db.session.commit()
+        return {'message':'Category deleted successfully'},200
+        
+        
